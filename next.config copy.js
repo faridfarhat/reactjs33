a7e90edda0809956fb17b1b2fc6294de02463364
faridfarhat <93440204+faridfarhat@users.noreply.github.com>
2026-01-28 21:03:47 +1300

    Update next.config copy.js

--- next.config copy.js
+++ next.config copy.js
@@ -1,14 +1,26 @@
-/** @type {import('next').NextConfig} */
-const basePath = process.env.BASE_PATH || process.env.NEXT_PUBLIC_BASE_PATH || '/reactjs';
+const { basePath } = require('../../next-react-project3/next.config.mjs');
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const isProd = process.env.NODE_ENV = 'production';
 
 const nextConfig = {
+  basePath : isProd ? 'reactjs33' : '',
   output: 'export',
-  basePath: basePath,
-  assetPrefix: basePath,
+  distDir: 'dist',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
-};
-
-module.exports = nextConfig;
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
